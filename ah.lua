5d88289e9868a611114579ab94f22427cf863132
Kingcurry650 <42081529+Kingcurry650@users.noreply.github.com>
2022-12-07 19:51:28 -0600

    Update ah.lua

--- ah.lua
+++ ah.lua
@@ -1,6 +1,6 @@
 local ont = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()
     end
-ont("loadstring(game:HttpGet(('https://raw.githubusercontent.com/Korabi-dev/roblox-crasher/main/ah.lua'),true))()")
+ont("loadstring(game:HttpGet(('https://raw.githubusercontent.com/Kingcurry650/Crash-Script/main/ah.lua'),true))()")
 
 
 function log(msg, color) 
